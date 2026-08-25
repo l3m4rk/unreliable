13f0b5f7981f0f0fe84acc9916fc88ef262d35fc
L3m4rk <b4r4n0v@gmail.com>
2026-08-25 19:40:35 +0300

    chore: initialize Spring Boot project

---
+++ src/main/java/dev/l3m4rk/unreliable/UnreliableApplication.java
@@ -0,0 +1,13 @@
+package dev.l3m4rk.unreliable;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnreliableApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UnreliableApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/l3m4rk/unreliable/UnreliableApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.l3m4rk.unreliable;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnreliableApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
